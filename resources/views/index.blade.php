bb640770a678901d1554467cb918a6bdb3e3ee87
Jaewon Kim <epiloum@gmail.com>
2021-03-30 00:27:38 +0900

    fix: add meta tag on index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <title>성경읽기표 - 대길교회 청년부</title>
     <style type="text/css">
         * {
