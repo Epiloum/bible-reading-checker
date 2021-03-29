1c5f3741a12abb62d176910a44d40ee94b99770d
Jaewon Kim <epiloum@gmail.com>
2021-03-30 00:25:02 +0900

    feat: index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>성경읽기표 - 대길교회 청년부</title>
+    <style type="text/css">
+        * {
+            margin : 0;
+            padding: 0;
+        }
+        html {
+            height: 100%;
+        }
+        body {
+            height: 100%;
+            background-image: url('/images/index.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center bottom;
+            text-align: center;
+        }
+        hgroup {
+            height: 75%;
+            min-height: 80px;
+            font-family: 'sans-serif', 'Malgun Gothic';
+            letter-spacing: -2px;
+        }
+        h1 {
+            padding-top: 20%;
+            color: #333;
+            font-size: 52px;
+        }
+        h2 {
+            padding-top: 10px;
+            color: #666;
+            font-size: 30px;
+            font-weight: normal;
+
+        }
+        a {
+            display: inline-block;
+            width: 183px;
+            height: 43px;
+            background-image: url('/images/kakao_login_large_narrow.png');
+            background-size: 183px 43px;
+            background-repeat: no-repeat;
+            box-shadow: 5px 5px 20px rgba(50, 20, 0, .7);
+            overflow: hidden;
+            font-size: 0;
+            text-indent: -5000px;
+        }
+    </style>
+</head>
+<body>
+<hgroup>
+    <h1>성경읽기표</h1>
+    <h2>대길교회 청년부</h2>
+</hgroup>
+<a href="/social/kakao">카카오로그인</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('social/kakao');
+    return view('index');
 });
 
 Route::get('/social/{provider}', [
