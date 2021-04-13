a1c8d244f8f874a835eb3f0b98104826aa94e164
Jaewon Kim <epiloum@gmail.com>
2021-04-13 23:51:12 +0900

    feat: hide mobile input

--- resources/views/layers/settings.blade.php
+++ resources/views/layers/settings.blade.php
@@ -1,7 +1,8 @@
 <div id="layerSettings">
     <form id="frmSettings">
+        <input type="hidden" name="mobile" value="" />
         <h2>프로필 설정</h2>
-        <blockquote>입력해주신 소속과 이름, 휴대전화 번호는 매월 추첨으로 선물을 드릴 때 이용합니다.</blockquote>
+        <blockquote>입력해주신 정보는 청년부 예배에서 매월 추첨으로 선물을 드릴 때 이용합니다.</blockquote>
         <ul>
             <li>
                 <label><input type="radio" name="division" value="청년1부" />청년1부</label>
@@ -10,7 +11,7 @@
             <li>
                 <input type="text" name="name" placeholder="이름을 입력해주세요" />
             </li>
-            <li><input type="text" name="mobile" placeholder="휴대전화 번호를 입력해주세요" /></li>
+            <?/*<li><input type="text" name="mobile" placeholder="휴대전화 번호를 입력해주세요" /></li>*/?>
         </ul>
         <div>
             <input type="submit" value="저장하기" />
