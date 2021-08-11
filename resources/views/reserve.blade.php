6ebcec8bc0aed58dc247edb35bda32d1f76b6243
Jaewon Kim <epiloum@gmail.com>
2021-08-11 18:59:00 +0900

    fix: init the router and view

---
+++ resources/views/reserve.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ko" class="os_window">
+<head>
+    <meta charset="utf-8"/>
+    <title>Daum</title>
+</head>
+<body>
+<div style="text-align:center; line-height:1.7em; margin-top:120px">
+    <img src="http://daegil.net/images/logo.png" alt="대길교회" /><br /><br />
+    <b>8/15(주일) 청년부 현장예배 예약</b>은 아직 시작되지 않았습니다!<br />
+    8/14 오후 1시부터 신청할 수 있습니다.
+</div>
+</body>
+</html>
